3acbfeb952d190fc64bb907e1fcc9f44343b102d
HyLee-LHY <33343907+HyLee-LHY@users.noreply.github.com>
2022-07-17 19:06:47 +0800

    Test

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
 from kivy.app import App
-from kivy.graphics import Line, Color
-from kivy.uix.widget import Widget
+from kivy.uix.button import Button
 
-
-class DrawCanvasWidget(Widget):
-    def __init__(self,**kwargs):
-        super().__init__(**kwargs)
-
-
-class PaintApp(App):
-    def build(self):
-        self.draw_canvas_widget = DrawCanvasWidget()
-
-        return self.draw_canvas_widget  # 返回root控件
-
-if __name__ == "__main__":
-    PaintApp().run()
+class MainApp(App):
+  def build(self):
+    return Button(text="Hello World")
+    
+MainApp().run()
